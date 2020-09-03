a54af00803345ce530bee0d344c0bad67768382b
Petur Saemundsen <petursam@gmail.com>
2020-09-03 10:18:45 +0000

    Algorithm written for max_int

--- max_int.py
+++ max_int.py
@@ -1,4 +1,7 @@
+#input a number
+#if it is less than zero it stops
 num_int = int(input("Input a number: "))
 
-if num_int < 0:
-    
+while num_int < 0:
+    num_int = int(input("Input a number: "))
+print(end="")
